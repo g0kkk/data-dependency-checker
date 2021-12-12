acc4926154bdad3b8981f1e3afe63519c6ca2ec7
g0kk <gkgokulkrishna33@gmail.com>
2021-12-11 19:29:14 -0700

    adding setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='ddc',
+    version='1.0.1',
+    description="ddc",
+    author="Gokul",
+    author_email="gpraveen@asu.edu",
+    install_requires=[
+        'angr',
+        'logging'
+    ]
+)
